48343ea4090eccb39e1f64651ebfa1d4f50d5a36
Dan Pasette <dan@10gen.com>
2017-05-10 18:31:37 -0400

    Updated import to new pymongo, scraper to new format and updated the data

--- util/get_buoys.py
+++ util/get_buoys.py
@@ -18,7 +18,12 @@ buoy_csv.writerow(['buoy_id', 'latitude', 'longitude', 'description'])
 
 # parse the all buoys page into individual links to each buoy
 all_buoys_url = os.path.join(NOAA_ROOT, "to_station.shtml")
-all_buoys_page = urllib2.urlopen(all_buoys_url)
+print "Parsing all buoys page: %s" % all_buoys_url
+try:
+    all_buoys_page = urllib2.urlopen(all_buoys_url, timeout=5)
+except:
+    print "ERROR: Timed out opening all buoys page %s" % all_buoys_url
+
 if not all_buoys_page:
     print "ERROR: Error opening all buoys page %s" % all_buoys_url
     exit(1)
@@ -31,9 +36,9 @@ if not tags:
 
 # compile some regex's that we'll need to parse each page
 buoy_re = re.compile('.*href="(.*?)">(.*)<.*')
-lat_re = re.compile('.*var stnlat = (.*?);.*') 
-lon_re = re.compile('.*var stnlon = (.*?);.*') 
-desc_re = re.compile('.*?- (.*?)</h1>')
+lat_re = re.compile('.*var currentstnlat = (.*?);.*')
+lon_re = re.compile('.*var currentstnlng = (.*?);.*')
+desc_re = re.compile('.*var currentstnname = (.*?);.*')
 
 # parse out the individual buoy id and page location then get the buoy info
 for buoy in tags:
@@ -41,12 +46,19 @@ for buoy in tags:
     if not m:
         print "ERROR: regex failed for this buoy page: %s" % buoy
         continue
-    buoy_page_url = os.path.join(NOAA_ROOT, m.group(1))   
+    buoy_page_url = os.path.join(NOAA_ROOT, m.group(1))
     buoy_id = m.group(2)
 
     # using each buoy id, get the location, name, etc.
     print 'Parsing: %s' % buoy_page_url
-    buoy_soup = BeautifulSoup(urllib2.urlopen(buoy_page_url))
+    try:
+        buoy_soup = BeautifulSoup(urllib2.urlopen(buoy_page_url, timeout=5))
+    except:
+        print "ERROR: Timed out opening this buoy: %s" % buoy_page_url
+
+    if not buoy_soup:
+        print "ERROR: Couldn't load this buoy"
+        continue
     script = buoy_soup.findAll(text=re.compile('.*stnlat.*'))
     m = lat_re.match(str(script))
     if not m:
@@ -59,11 +71,13 @@ for buoy in tags:
         continue
     longitude = m.group(1)
 
-    location_tag = buoy_soup.find('h1', attrs={"style" : "text-align:center; margin:3px;"})
-    m = desc_re.match(str(location_tag))
+    m = desc_re.match(str(script))
     description = ''
     if m:
         description = m.group(1)
+    else:
+        print "WARNING: No description data for this buoy"
+
     buoy_csv.writerow([buoy_id, latitude, longitude, description])
 
 print "All buoy locations parsed"
--- util/import_buoys.py
+++ util/import_buoys.py
@@ -5,14 +5,15 @@ Add geospatial index.
 """
 
 import csv
-from pymongo import Connection, GEO2D
+import pymongo
 
 INPUT_FILE = "data/all_buoys.csv"
 
 buoy_csv = csv.reader(open(INPUT_FILE))
 header = buoy_csv.next()
 
-connection = Connection()
+connection = pymongo.MongoClient("mongodb://dan:dan1275@surflog-shard-00-00-u9fko.mongodb.net:27017,surflog-shard-00-01-u9fko.mongodb.net:27017,surflog-shard-00-02-u9fko.mongodb.net:27017/foo?ssl=true&replicaSet=surflog-shard-0&authSource=admin")
+
 db = connection.surf_log
 buoys = db.buoys
 
@@ -21,12 +22,12 @@ for row in buoy_csv:
             'loc': (float(row[2]), float(row[1])),
             'description': row[3],
             'online': True}
-    buoys.update({'buoy_id': '%s' % row[0]}, buoy, upsert=True)
+    buoys.update({'_id': '%s' % row[0]}, buoy, upsert=True)
     status = db.last_status()
     if not status.get("updatedExisting"):
         print "Inserted new buoy %s" % row[0]
     else:
         print "Updated buoy %s" % row[0]
-        
-buoys.create_index([("loc", GEO2D)])
+
+buoys.create_index([("loc", pymongo.GEOSPHERE)])
 
